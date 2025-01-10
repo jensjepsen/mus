4e1815d1388347ae583a18750c87bf6c063699e6
Jens Jepsen <jjqx@novonordisk.com>
2025-01-09 20:56:41 -0500

    fix: deref schemas

--- src/mus/functions.py
+++ src/mus/functions.py
@@ -12,6 +12,17 @@ def tool(**metadata: t.Dict[str, t.Any]):
         return func
     return decorator
 
+def remove_keys(obj: t.Any, keys: set):
+    if isinstance(obj, dict):
+        return {
+            key: remove_keys(val, keys)
+            for key, val
+            in obj.items()
+            if not key in keys
+        }
+    else:
+        return obj
+
 def get_schema(name: str, fields: t.List[tuple[str, t.Type]]):
     model_fields = {}
     for field_name, type in fields:
@@ -21,9 +32,11 @@ def get_schema(name: str, fields: t.List[tuple[str, t.Type]]):
             description = None
         model_fields[field_name] = (type, pydantic.Field(..., description=description))
     temp_model: t.Type[pydantic.BaseModel] = pydantic.create_model(name, **model_fields)
-    schema = temp_model.model_json_schema(mode="serialization")
-    schema = json.loads(json.dumps(jsonref.replace_refs(schema)))
-    return schema
+    schema = temp_model.model_json_schema()
+    dereffed = jsonref.replace_refs(schema)
+    cleaned = remove_keys(dereffed, {"$defs"})
+    
+    return cleaned
 
 def functions_map(functions: t.List[ToolCallableType]) -> t.Dict[str, ToolCallableType]:
     return {func.__name__: func for func in (functions or [])}
