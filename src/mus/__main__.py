46b6c510e33b24fed3efac3836be9edf5aea6261
Jens Jepsen <jjqx@novonordisk.com>
2025-03-04 19:50:11 +0100

    feat: optional dependencies

--- src/mus/__main__.py
+++ src/mus/__main__.py
@@ -1,5 +1,8 @@
 from . import run_file
 
 if __name__ == "__main__":
-    import typer
+    try:
+        import typer
+    except ImportError:
+        raise ImportError("Please install the cli dependencies by running `pip install mus[cli]`")
     typer.run(run_file)
