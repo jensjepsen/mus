449e4cc00ca8808603ea4bf62fbe7400d875c2f5
Jens Jepsen <jjqx@novonordisk.com>
2025-03-25 19:44:14 +0100

    feat: extism

--- scripts/build_guest.sh
+++ scripts/build_guest.sh
@@ -2,5 +2,5 @@
 
 # This script is used to build the guest wasm module
 
-pip install -e --target wasm_deps .
+pip install --target wasm_deps -e .
 PYTHONPATH=./wasm_deps extism-py src/mus/guest.py -o guest.wasm
