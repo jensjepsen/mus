e332590616ec7254dce2931d0d8a750e7f072878
Jens Jepsen <jjqx@novonordisk.com>
2025-03-28 13:54:31 +0100

    feat: sandbox

--- scripts/build_guest.sh
+++ scripts/build_guest.sh
@@ -2,8 +2,9 @@
 
 # This script is used to build the guest wasm module
 
+python -m pip install --target wasm_deps --platform any --platform wasi_0_0_0_wasm32 --python-version "3.12" --only-binary :all: --index-url https://benbrandt.github.io/wasi-wheels/ --extra-index-url https://pypi.org/simple pydantic-core
 python -m pip install --target wasm_deps -e .
-python -m pip install --force-reinstall --target wasm_deps --platform any --platform wasi_0_0_0_wasm32 --python-version "3.12" --only-binary :all: --index-url https://benbrandt.github.io/wasi-wheels/ --extra-index-url https://pypi.org/simple --upgrade pydantic-core
+
 
 echo "Building guest wasm module"
 cp -r src/mus wasm_deps/mus
