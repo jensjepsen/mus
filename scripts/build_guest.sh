3cc2aacb7b7e9113303c6ee49d229813dff56907
Jens Jepsen <jjqx@novonordisk.com>
2025-03-30 16:43:21 +0200

    fix: sandbox

--- scripts/build_guest.sh
+++ scripts/build_guest.sh
@@ -4,14 +4,9 @@
 
 curl -Ls https://raw.githubusercontent.com/extism/python-pdk/main/install.sh | bash
 
-rm -r wasm_deps
-mkdir wasm_deps
-uv pip compile pyproject.toml -o wasm_deps/temp-reqs.txt
-uvx --native-tls pip install --upgrade --force --target wasm_deps --platform any --platform wasi_0_0_0_wasm32 --python-version "3.12" --only-binary :all: --index-url https://benbrandt.github.io/wasi-wheels/ --extra-index-url https://pypi.org/simple pydantic-core
-uvx --native-tls pip install -r wasm_deps/temp-reqs.txt
-ls wasm_deps
+python -m pip install --platform any --platform wasi_0_0_0_wasm32 --python-version "3.12" --only-binary :all: --target wasm_deps https://github.com/benbrandt/wasi-wheels/releases/download/pydantic-core%2Fv2.33.0/pydantic_core-2.33.0-cp312-cp312-wasi_0_0_0_wasm32.whl
 
 echo "Building guest wasm module"
 cp -r src/mus wasm_deps/mus
 ls wasm_deps
-PYTHONPATH=wasm_deps extism-py src/mus/guest/main.py -o guest.wasm
+PYTHONPATH=wasm_deps extism-py test.py -o guest.wasm
