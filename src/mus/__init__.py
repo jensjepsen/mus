11a822669d44d3ce29e0fc9d11ee647bff8bb198
Jens Jepsen <jjqx@novonordisk.com>
2025-04-05 22:59:34 +0200

    fix: don't import sandbox functionality when in a sandbox

--- src/mus/__init__.py
+++ src/mus/__init__.py
@@ -1,14 +1,16 @@
 from .main import Mus
 from .llm.types import File, Query, ToolUse, ToolResult, Assistant
 from .llm.llm import IterableResult
+import sys
 
-
-try:
-    import extism
-except ImportError:
-    pass
-else:
-    from ._sandbox import sandbox
+if not "extism" in sys.modules:
+    # If extism is already imported, we're probably in a WASM env already
+    try:
+        import extism
+    except ImportError:
+        pass
+    else:
+        from ._sandbox import sandbox
 
 try:
     import anthropic
