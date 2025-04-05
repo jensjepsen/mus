cfd17aa94f19f53dcac1dda8af758fcbc4e91db7
Jens Jepsen <jjqx@novonordisk.com>
2025-04-05 23:02:25 +0200

    chore: update extism comment

--- src/mus/__init__.py
+++ src/mus/__init__.py
@@ -3,8 +3,10 @@ from .llm.types import File, Query, ToolUse, ToolResult, Assistant
 from .llm.llm import IterableResult
 import sys
 
+# If extism is already imported, we're probably in a WASM env already
+# so we can't import it again
+# and we can't nest sandboxes
 if not "extism" in sys.modules:
-    # If extism is already imported, we're probably in a WASM env already
     try:
         import extism
     except ImportError:
